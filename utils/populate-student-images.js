8861d3bfda73175dd0efeec65c1b322e9cb973d0
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-16 13:15:37 -0500

    Add a script to pull student images and populate fixtures directory with them.

--- utils/populate-student-images.js
+++ utils/populate-student-images.js
@@ -1,12 +1,17 @@
 const fs = require('fs');
+const path = require('path');
 const request = require('request');
 
-const download = function(uri, filename, callback){
+const studentImgPath = path.join(__dirname, '../react-ui/src/fixtures/student-images');
+const randomUserApiURL = 'https://randomuser.me/api/portraits';
+
+const download = function(uri, filename){
   request.head(uri, function(err, res, body) {
-    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', () => {});
   });
 };
 
-download('https://randomuser.me/api/portraits/women/28.jpg', 'test.png', function(){
-  console.log('done');
-});
+for (let i = 0; i < 10; i++) {
+  download(`${randomUserApiURL}/men/${i + 1}.jpg`, `${studentImgPath}/men-${i + 1}.jpg`);
+  download(`${randomUserApiURL}/women/${i + 1}.jpg`, `${studentImgPath}/women-${i + 1}.jpg`);
+}
